2bc3172489729ae22c042f0f6286c192dd8dfa71
RaAndOn <jraanan@andrew.cmu.edu>
2019-12-04 13:17:52 -0500

    Varaiable coverage (#14)
    * Setup pipeline for variance from server
    
    * Change variance to coverage side
    
    * Fix cluster and variance assignment

--- src/litter_localization/scripts/save_gps.py
+++ src/litter_localization/scripts/save_gps.py
@@ -11,27 +11,29 @@ import yaml
 
 # Set global variables
 params = ext.get_params_dictionary()
-json_file_path = os.path.join(params["sureclean_server_path"], params["gps_json_path"])
+json_file_path = os.path.join(
+    params["sureclean_server_path"], params["gps_json_path"])
+
 
 def save_gps_from_topic():
-### This function saves a single message from a gps topic to a bag ###
+    ### This function saves a single message from a gps topic to a bag ###
     # Start node
     rospy.init_node('uav_gps_listener')
 
     # Delete bag if one exists
     if os.path.exists(json_file_path):
         os.remove(json_file_path)
-    
+
     # Wait for next message on topic
     gps_data_msg = rospy.wait_for_message(params["gps_topic"], NavSatFix)
 
     # Print details of GPS data
-    rospy.loginfo("Latitude: %s",gps_data_msg.latitude)
-    rospy.loginfo("Longitude %s",gps_data_msg.longitude)
+    rospy.loginfo("Latitude: %s", gps_data_msg.latitude)
+    rospy.loginfo("Longitude %s", gps_data_msg.longitude)
     rospy.loginfo("Data Saved To: %s", json_file_path)
 
     # GPS data
-    gps_data = [[gps_data_msg.latitude,gps_data_msg.longitude]] 
+    gps_data = [[gps_data_msg.latitude, gps_data_msg.longitude]]
     save_gps_to_json(json_file_path, gps_data)
 
 
@@ -39,19 +41,20 @@ def save_gps_to_json(file_path, gps_data):
     data = {}
     for i in range(len(gps_data)):
         data[i] = ({
-			'GPS':
-				{
-				'Latitude':  gps_data[i][0],
-				'Longitude': gps_data[i][1],
-				}
-		})
+            'GPS':
+            {
+                'Latitude':  gps_data[i][0],
+                'Longitude': gps_data[i][1],
+                'Variance': gps_data[i][2]
+            }
+        })
 
     with open(file_path, 'w+') as outfile:
-	    json.dump(data, outfile, indent=2)
+        json.dump(data, outfile, indent=2)
 
 
 if __name__ == '__main__':
     try:
-       save_gps_from_topic()
+        save_gps_from_topic()
     except rospy.ROSInterruptException:
-	    pass
+        pass
