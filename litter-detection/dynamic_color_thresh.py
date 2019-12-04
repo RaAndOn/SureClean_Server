377fc1ed87c4ec0539417499a6f070a29eb100b3
RaAndOn <jraanan@andrew.cmu.edu>
2019-12-04 13:15:34 -0500

    Merge FVD Into Master (#13)
    * Added dataset.
    
    * Updated .gitignore.
    
    * Updated .gitignore
    
    * Updated .gitignore.
    
    * Added v1 of litter detection pipeline.
    
    * Updated .gitignores
    
    * Updated thresholder
    
    * Added file headers.
    
    * Updated bootstrap number
    
    * Updated SVM model.
    
    * Addressed PR feedback from Josh. Changed variables to snake case and added function headers.
    
    * Detector output (#3)
    
    * Updated .gitignore
    
    * Begun testing reid logic
    
    * Implemented RANSAC affine fitter + visualization
    
    * Have 2D synthetic data visualization
    
    * LFS tracks image files
    
    * Removed REID code
    
    * Updated data augmenter directories
    
    * Renamed svd_images/ to images/
    
    * Updated .gitignore
    
    * Updated .gitignores
    
    * Abanded git-lfs idea due to impracticality.
    
    * Rearranged and renamed model files
    
    * Saved new SVM model. Modified detector to save output to JSON file.
    
    * Removed erraneous comments in function header
    
    * Addressed PR feedback
    
    * Updated order in which points are saved to remove duplicates before visualization
    
    * Communication pipeline (#5)
    
    * Move contents of 'server_litter_pipeline' to new branch to resolve merge conflicts
    
    * Add pixel to utm back in
    
    * Fix gps topic
    
    *  Rearrange imports
    
    * Setup OpenCV 2 in ROS Package
    
    * Use rospkg tosetup absolute paths instead of relative paths
    
    * Update shebang
    
    * Don't import dynamic_color_thresholding yet
    
    * Break up extract_data
    
    * Fix std dev bug
    
    * Add plotting functionality
    
    * Integrate in litter-detection
    
    * Update plotting
    
    * Rename files switch to json paradim
    
    * Reduce repeated code
    
    * Rename save_gps
    
    * Add ability to move ugv
    
    * Fix publishing bug
    
    * Clean up code
    
    * Change litter-detection to work with litter pipeline
    
    * Rearrange covariance check and litter check
    
    * Fix topic name
    
    * Fix topic
    
    * Remove unecessary litter_localization/ImageMsg
    
    * Don't use NaN covariances
    
    * be able to plot all data
    
    * Remove dead code
    
    * Remove print line
    
    * Fix spelling
    
    * Remove unecessary return of image
    
    * Modify publish goal for returning to base (#6)
    
    * Sanitize input of litter pipeline (#7)
    
    * CHECKPOINT - before merging in clustering
    
    * Clustering (#8)
    
    * Imported packages for clustering
    
    * Added clustering and visualizations for debugging
    
    * Clustering bias
    
    * Modified criteria for clustering points. Now thresholding on covariance
    
    * Added variance computation
    
    * last change (#10) (#11)
    
    * Make debug plot axes equal (#12)

---
+++ litter-detection/dynamic_color_thresh.py
@@ -0,0 +1,233 @@
+"""
+Takes in images from the images/input/ directory, finds the litter objects in the images
+and saves the output to the images/output/ directory. A JSON file containing the results
+is also generated.
+
+Author: Atulya Ravishankar
+Updated: 09/08/2019
+"""
+
+import pickle
+import os
+import copy
+import numpy as np
+import json
+import cv2
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib import colors
+
+from hog_svm import Dataset
+
+debug_thresh = False
+debug_file = ""
+
+data = Dataset(split=0.95)
+
+# OpenCV range for HSV channels:
+# H: [0 180]
+# S: [0 255]
+# V: [0 255]
+
+class LitterDetector(object):
+
+    # def __init__(self):
+    def __init__(self, path=""):
+        self.h_thresh_width = 4.5
+        self.s_thresh_width = 4.5
+        self.blob_lower_size = 5
+        self.blob_upper_size = 150
+        self.patch_size = 30
+        self.img_width = 1280 
+        self.img_height = 720
+        self.svm_coeffs = np.load(path + "model/grass/grass_svm_coeffs_lowres.npy")
+        self.svm_intercept = np.load(path + "model/grass/grass_svm_intercept_lowres.npy")
+        self.svm_pkl_file = path + "model/grass/grass_svm_model_lowres.pkl"
+        with open(self.svm_pkl_file, 'rb') as file:
+            self.clf = pickle.load(file)
+        if (self.clf is None):
+            raise Exception("Failed to load SVM model.")
+    
+    '''
+    Reads in an image 'fileName' and outputs the image in BGR, RGB and HSV color spaces.
+    '''
+    def read_image(self, fileName):
+        img_bgr = cv2.imread(fileName)
+        img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+        img_hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+        return (img_bgr, img_rgb, img_hsv)
+    
+    '''
+    Computes dynamic thresholds in the HSV space for the image 'img_hsv'.
+    '''
+    def compute_thresholds(self, img_hsv):
+        [H, S, _] = [img_hsv[:, :, 0], img_hsv[:, :, 1], img_hsv[:, :, 2]]
+        thresh = dict()
+        H_mean = np.mean(H)
+        H_std = np.std(H)
+        S_mean = np.mean(S)
+        S_std = np.std(S)
+        thresh["H_min"] = H_mean - self.h_thresh_width*H_std
+        thresh["H_max"] = H_mean + self.h_thresh_width*H_std
+        thresh["S_min"] = S_mean - self.s_thresh_width*S_std
+        thresh["S_max"] = 255
+        thresh["V_min"] = 0
+        thresh["V_max"] = 255
+        return thresh
+    
+    '''
+    Compute the centroid of a contour 'contour'.
+    '''
+    def get_contour_centroid(self, contour):
+        M = cv2.moments(contour)
+        try:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+        except:
+            cX = 0
+            cY = 0
+        return (cX, cY)
+    
+    '''
+    Extracts a patch from the image centered at the centroid of 'ctr'.
+    '''
+    def get_patch_idx(self, ctr):
+        (cX, cY) = self.get_contour_centroid(ctr)
+        halfPatch = int(self.patch_size/2)
+        if (cX < halfPatch):
+            minX = 0
+            maxX = minX + self.patch_size - 1
+        elif (cX > self.img_width-halfPatch):
+            maxX = self.img_width - 1
+            minX = maxX - self.patch_size + 1
+        else:
+            minX = cX - halfPatch
+            maxX = minX + self.patch_size - 1
+        if (cY < halfPatch):
+            minY = 0
+            maxY = minY + self.patch_size - 1
+        elif (cY > self.img_height-halfPatch):
+            maxY = self.img_height - 1
+            minY = maxY - self.patch_size + 1
+        else:
+            minY = cY - halfPatch
+            maxY = minY + self.patch_size - 1
+        return (minX, maxX, minY, maxY)
+    
+    '''
+    Classifies an image patch 'patch' as litter or not-litter.
+    '''
+    def predict(self, patch):
+        global data
+        img = patch[:, :, 0]
+        feat = data.get_feature(img)
+        label_clf = self.clf.predict(feat.reshape((1, -1)))
+        return label_clf[0]
+    
+    '''
+    Determines if a contour 'ctr' is valid (i.e. should be classified).
+    '''
+    def is_valid_contour(self, img_hsv, ctr):
+        global data
+        size_of_blob = ctr.shape[0]
+        if (size_of_blob >= self.blob_lower_size and size_of_blob <= self.blob_upper_size):
+            (x1, x2, y1, y2) = self.get_patch_idx(ctr)
+            patch = img_hsv[y1:y2+1, x1:x2+1, :]
+            return (self.predict(patch) > 0.5)
+        return False
+
+    '''
+    Thresholds the image 'img_hsv' based on the thresholds stored in 'thresh'.
+    '''
+    def threshold_image(self, img_hsv, thresh):
+        global debug_thresh, debug_file
+        img_thresh = 255 - cv2.inRange(img_hsv, (thresh["H_min"], thresh["S_min"], thresh["V_min"]), (thresh["H_max"], thresh["S_max"], thresh["V_max"]))
+        _, contours, _ = cv2.findContours(img_thresh, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE) 
+        mask = np.ones(img_thresh.shape[:2], dtype="uint8") * 255
+        valid_contours = []
+        for c in contours:
+            if self.is_valid_contour(img_hsv, c):
+                valid_contours.append(c)
+            else:
+                cv2.drawContours(mask, [c], -1, 0, -1)
+        image = cv2.bitwise_and(img_thresh, img_thresh, mask=mask).astype('uint8')
+        output = (255*image).astype("uint8")
+        if (debug_thresh):
+            cv2.imwrite("images/thresh/"+ debug_file + "_thresh.jpg", img_thresh)
+        return (output, valid_contours)
+    
+    '''
+    Plots the locations of litter ('contours') identified in 'image'.
+    '''
+    def visualize_litter_locations(self, image, points):
+        for [cX, cY] in points:
+            cv2.circle(image, (cX, cY), 50, (0, 0, 255), 3, 8, 0)
+        return image
+
+'''
+Takes in a list of points (x, y) that returns a subset of that list such that no
+two points are within 5px of each other.
+'''
+def filter_points(contours, ld):
+    filtered = []
+    to_remove = [False for _ in contours]
+    for i in range(len(contours)):
+        if not to_remove[i]:
+            for j in range(len(contours)):
+                if (i != j):
+                    (cXi, cYi) = ld.get_contour_centroid(contours[i])
+                    (cXj, cYj) = ld.get_contour_centroid(contours[j])
+                    dist = np.linalg.norm(np.array([cXi, cYi]) - np.array([cXj, cYj]))
+                    if (dist < 10):
+                        to_remove[j] = True
+            filtered.append(contours[i])
+    final_positions = []
+    for ctr in filtered:
+        final_positions.append(ld.get_contour_centroid(ctr))
+    return final_positions
+
+'''
+Uses the litter detector results, filters them and saves it to a JSON file.
+'''
+def filter_and_save_results(results):
+    with open('results.json', 'w') as results_file:
+        json.dump(results, results_file)
+
+'''
+This function is externally accessible to be run by the litter detection pipeline
+'''
+def run_dynamic_color_thresh(file_path, sureclean_server_path): 
+    file = file_path.split('/')[-1].split('.')[0]
+    ld = LitterDetector(sureclean_server_path+'/litter-detection/')
+    results = dict()
+    (img_bgr, img_rgb, img_hsv) = ld.read_image(file_path)
+    thresh = ld.compute_thresholds(img_hsv)
+    (img_bin, contours) = ld.threshold_image(img_hsv, thresh)
+    # Filter points to remove duplicates
+    results[file] = filter_points(contours,ld)
+    output = ld.visualize_litter_locations(img_bgr, results[file])
+    cv2.imwrite(sureclean_server_path+'/litter-detection/'+"images/output/"+file+"_result.jpg", output)
+    return results
+
+
+if __name__ == "__main__":
+    idx = 1
+    file_names = os.listdir("images/input/")
+    ld = LitterDetector()
+    results = dict()
+    for file in file_names:
+        if (file != ".DS_Store" and file != ".gitignore"):
+            debug_file = file[:-4]
+            print("Processing file %d of %d" %(idx, len(fileNames)-2))
+            fileName = "images/input/" + file
+            (img_bgr, img_rgb, img_hsv) = ld.read_image(fileName)
+            thresh = ld.compute_thresholds(img_hsv)
+            (img_bin, contours) = ld.threshold_image(img_hsv, thresh)
+            # Filter points to remove duplicates
+            results[file] = filter_points(contours,ld)
+            output = ld.visualize_litter_locations(img_bgr, results[file])
+            cv2.imwrite("images/output/"+file[:-4]+"_result.jpg", output)
+            idx += 1
+    filter_and_save_results(results)
+    print("Done")
---
+++ litter-detection/hog_svm.py
@@ -0,0 +1,180 @@
+"""
+Manager SVM classifier
+- Training: uses training data and saves the model to disk.
+- Testing/Predicting: uses saved model to classify test image(s)
+
+Author: Atulya Ravishankar
+Updated: 09/08/2019
+"""
+
+import pickle
+import os
+import random
+import math
+import numpy as np
+import cv2
+from skimage.feature import hog
+from sklearn.svm import SVC, LinearSVC
+from sklearn.metrics import classification_report,accuracy_score
+from matplotlib import pyplot as plt
+
+# NOTE: The dataset needed to train the SVM is not in the Git repository due to its immense size.
+#       Contact Atulya if you would like the dataset.
+
+class Dataset(object):
+
+    def __init__(self, split=0.75):
+        # Data hyperparameters
+        self.split = split
+        self.pos_dir = "dataset/grass/augmented/pos/"
+        self.neg_dir = "dataset/grass/augmented/neg/"
+        self.ignore_file = ".DS_Store"
+        # pos_files = os.listdir(self.pos_dir)
+        # neg_files = os.listdir(self.neg_dir)
+        # random.shuffle(pos_files)
+        # random.shuffle(neg_files)
+        # pos_idx = int(len(pos_files) * self.split)
+        # neg_idx = int(len(neg_files) * self.split)
+        # (self.pos_train, self.pos_test) = (pos_files[:pos_idx], pos_files[pos_idx:])
+        # (self.neg_train, self.neg_test) = (neg_files[:neg_idx], neg_files[neg_idx:]) 
+        # self.num_training_images = len(self.pos_train) + len(self.neg_train)
+        # self.num_testing_images = len(self.pos_test) + len(self.neg_test)
+        self.img_dim = 70
+        # HOG hyperparameters
+        self.hog_orientations = 9
+        self.hog_pixels_per_cell = (6, 6)
+        self.hog_cells_per_block = (3, 3)
+        self.hog_block_norm = "L1"
+        self.hog_visualize = False
+        self.hog_transform_sqrt = False
+        self.hog_feature_vector = True
+        self.hog_size = 729
+        # SVM hyperparameters
+        self.svm_coeff_file = "model/grass/grass_svm_coeffs_lowres.npy"
+        self.svm_intercept_file = "model/grass/grass_svm_intercept_lowres.npy"
+        self.svm_pkl_file = "model/grass/grass_svm_model_lowres.pkl"
+    
+    '''
+    Loads training data (images and labels) for classifier.
+    '''
+    def get_training_data(self):
+        print("Getting training data")
+        idx = 0
+        training_data = np.zeros((self.img_dim, self.img_dim, self.num_training_images))
+        training_labels = np.zeros(self.num_training_images)
+        print("    Fetching positive samples")
+        for file in self.pos_train:
+            if (file != self.ignore_file):
+                file_name = self.pos_dir + file
+                img = cv2.imread(file_name)
+                if (img is not None):
+                    training_data[:, :, idx] = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)[:, :, 0]
+                    training_labels[idx] = 1
+                    idx += 1
+        print("    Fetching negative samples")
+        for file in self.neg_train:
+            if (file != self.ignore_file):
+                file_name = self.neg_dir + file
+                img = cv2.imread(file_name)
+                if (img is not None):
+                    training_data[:, :, idx] = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)[:, :, 0]
+                    training_labels[idx] = 0
+                    idx += 1
+        return training_data.astype('float'), training_labels
+    
+    '''
+    Loads testing data (images and labels) for classifier.
+    '''
+    def get_testing_data(self):
+        print("Getting test data")
+        idx = 0
+        testing_data = np.zeros((self.img_dim, self.img_dim, self.num_testing_images))
+        testing_labels = np.zeros(self.num_testing_images)
+        print("    Fetching positive samples")
+        for file in self.pos_test:
+            if (file != self.ignore_file):
+                file_name = self.pos_dir + file
+                img = cv2.imread(file_name)
+                if (img is not None):
+                    testing_data[:, :, idx] = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)[:, :, 0]
+                    testing_labels[idx] = 1
+                    idx += 1
+        print("    Fetching negative samples")
+        for file in self.neg_test:
+            if (file != self.ignore_file):
+                file_name = self.neg_dir + file
+                img = cv2.imread(file_name)
+                if (img is not None):
+                    testing_data[:, :, idx] = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)[:, :, 0]
+                    testing_labels[idx] = 0
+                    idx += 1
+        return testing_data.astype('float'), testing_labels
+    
+    '''
+    Converts an image patch into a HoG feature for the classifier.
+    '''
+    def get_feature(self, patch_H):
+        feat = hog(patch_H, cells_per_block=self.hog_cells_per_block,
+                        pixels_per_cell=self.hog_pixels_per_cell,
+                        orientations=self.hog_orientations,
+                        block_norm=self.hog_block_norm,
+                        transform_sqrt=self.hog_transform_sqrt,
+                        feature_vector=self.hog_feature_vector)
+        return feat
+
+    '''
+    Trains a Support Vector Machine (SVM) classifier and saves the model for future use.
+    '''
+    def train_svm(self):
+        print("Training SVM")
+        features = np.zeros((self.num_training_images, self.hog_size))
+        data, labels = self.get_training_data() # (100, 100, 1200), (1200,)
+        print("    Creating features")
+        for i in range(self.num_training_images):
+            img = data[:, :, i]
+            feat = self.get_feature(img)
+            features[i, :] = feat
+        print("    Computing weights")
+        clf = LinearSVC(random_state=0, tol=1e-5)
+        clf.fit(features.astype('float'), labels)
+        np.save(self.svm_coeff_file, clf.coef_)
+        np.save(self.svm_intercept_file, clf.intercept_)
+        with open(self.svm_pkl_file, 'wb') as file:  
+            pickle.dump(clf, file, protocol=2)
+        return clf
+    
+    '''
+    Tests a Support Vector Machine (SVM) model.
+    Model is loaded from disk.
+    '''
+    def test_svm(self):
+        print("Testing SVM")
+        with open(self.svm_pkl_file, 'rb') as file:  
+            clf = pickle.load(file)
+        features = np.zeros((self.num_testing_images, self.hog_size))
+        data, labels = self.get_testing_data()
+        print("    Creating features.")
+        for i in range(self.num_testing_images):
+            img = data[:, :, i]
+            feat = self.get_feature(img)
+            features[i, :] = feat
+        print("    Scoring")
+        acc = clf.score(features.astype('float'), labels)
+        print("Accuracy: %f" %acc)
+        return acc
+    
+    '''
+    Classifies an image patch using the classifier.
+    '''
+    def predict(self, patch, classifier):
+        img = patch[:, :, 0]
+        feat = self.get_feature(img)
+        label_clf = classifier.predict(feat.reshape((1, -1)))
+        return label_clf[0]
+
+
+if __name__ == "__main__":
+    data = Dataset(split=0.97)
+    data.train_svm()
+    data.test_svm()
+    
